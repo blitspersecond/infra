da4311bd20fba69516be78b0178f185f5d16fd73
Kris Holdich <kristian.holdich@cloudnc.com>
2024-03-11 17:23:40 +0000

    cloudflare

--- etc/provider.eu-west-1.tf
+++ etc/provider.eu-west-1.tf
@@ -7,8 +7,11 @@ terraform {
     aws = {
       source = "hashicorp/aws"
     }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
   }
-
   backend "s3" {
     encrypt        = true
     dynamodb_table = "terraform-state-locks"
